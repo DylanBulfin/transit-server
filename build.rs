3b418fe246d7a87feddf39cf2217465addd1385e
Dylan Bulfin <dylanbulfin@gmail.com>
2025-05-27 04:46:22 -0400

    Change buffer growth size

--- build.rs
+++ build.rs
@@ -1,4 +1,16 @@
+use std::{env, path::PathBuf};
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/db-transit.proto")?;
+    // tonic_build::compile_protos("proto/db-transit.proto")?;
+
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    let largebuf_copy = out_dir.join("smallbuf");
+    let _ = std::fs::create_dir(largebuf_copy.clone()); // This will panic below if the directory failed to create
+    tonic_build::configure()
+        .out_dir(largebuf_copy)
+        .codec_path("crate::common::LargeBufferCodec")
+        .compile_protos(&["proto/db-transit.proto"], &["proto"])?;
+
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,30 @@
+use std::marker::PhantomData;
+
+use prost::Message;
+use tonic::codec::{BufferSettings, Codec, ProstCodec};
+
 pub mod diff;
 pub mod error;
 pub mod shared;
+
+pub struct LargeBufferCodec<T, U>(PhantomData<(T, U)>);
+
+impl<T, U> Codec for LargeBufferCodec<T, U>
+where
+    T: Message + Send + 'static,
+    U: Message + Default + Send + 'static,
+{
+    type Encode = T;
+    type Decode = U;
+
+    type Encoder = <ProstCodec<T, U> as Codec>::Encoder;
+    type Decoder = <ProstCodec<T, U> as Codec>::Decoder;
+
+    fn encoder(&mut self) -> Self::Encoder {
+        ProstCodec::<T, U>::raw_encoder(BufferSettings::new(1024 * 1024, 8192))
+    }
+
+    fn decoder(&mut self) -> Self::Decoder {
+        ProstCodec::<T, U>::raw_decoder(BufferSettings::default())
+    }
+}
